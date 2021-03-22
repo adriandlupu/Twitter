6df126f381ce2eeadb67392e1e49c18080e9339a
adriandlupu <48430145+adriandlupu@users.noreply.github.com>
2021-03-22 18:44:33 +0200

    DbConnection class added

---
+++ twitter/src/main/java/org/fiipractic/config/DbConnection.java
@@ -0,0 +1,28 @@
+package org.fiipractic.config;
+
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DbConnection {
+    private static Connection con=null;
+    static {
+        String jdbcURL = "jdbc:mysql://localhost:3306/twitter";
+        String usernameConnection = "adrian.lupu";
+        String passwordConnection = "password";
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(jdbcURL, usernameConnection, passwordConnection);
+        }
+        catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection()
+    {
+        return con;
+    }
+}
--- twitter/src/main/java/org/fiipractic/service/UserService.java
+++ twitter/src/main/java/org/fiipractic/service/UserService.java
@@ -2,10 +2,10 @@ package org.fiipractic.service;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.taglibs.standard.lang.jstl.Logger;
+import org.fiipractic.config.DbConnection;
 import org.fiipractic.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,62 +14,33 @@ import org.fiipractic.exception.NotFoundException;
 
 @Component
 public class UserService {
-    ///private static final Logger LOGGER = Logger.get(UserService.class);
 
     private final List<User> users = new ArrayList<>();
-
-
+    Connection con= DbConnection.getConnection();
     public void create(User userFromRq) {
-        //logger.info("bullshit");
-        User user = new User();
-
-            String jdbcURL = "jdbc:mysql://localhost:3306/twitter";
-            String usernameConnection = "adrian.lupu";
-            String passwordConnection = "password";
+        //User user = new User();
         try {
-            //JdbcTemplate jdbcTemplateObject=new JdbcTemplate();
-            //DataSource dataSource;
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(jdbcURL, usernameConnection, passwordConnection);
-            Statement mystmt= connection.createStatement();
-            mystmt.executeUpdate("insert into user (userName,firstName,lastName,email,password) values ('"+userFromRq.getUserName()+"','"+userFromRq.getFirstName()+"','"+userFromRq.getLastName()+"','"+userFromRq.getEmail()+"','"+userFromRq.getPass()+"')");
+            Statement mystmt = con.createStatement();
+            mystmt.executeUpdate("insert into user (userName,firstName,lastName,email,password) values ('" + userFromRq.getUserName() + "','" + userFromRq.getFirstName() + "','" + userFromRq.getLastName() + "','" + userFromRq.getEmail() + "','" + userFromRq.getPass() + "')");
             //String sql="insert into user (userName,firstName) values(?,?)";
             //jdbcTemplateObject.update(sql,userFromRq.getUserName(),userFromRq.getFirstName());
             //return;
-        } catch (SQLException |ClassNotFoundException ex) {
-            // handle any errors
-            //user.setUserName(ex.getMessage().toString());
-            System.out.println("SQLException: {}" + ex.getMessage());
-        }
-
-
-        //user.setId((long) users.size() + 1);
-
-        //user.setFirstName("no");
-       // user.setLastName(userFromRq.getLastName());
-        //user.setEmail(userFromRq.getEmail());
-        //user.setPass(userFromRq.getPass());
-
-        //users.add(user);
+        }catch (SQLException e){
+            System.out.println(e.getMessage());}
     }
 
     public List<User> getAll() {
-        String jdbcURL = "jdbc:mysql://localhost:3306/twitter";
-        String usernameConnection = "adrian.lupu";
-        String passwordConnection = "password";
         users.clear();
         User user;
         try{
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(jdbcURL, usernameConnection, passwordConnection);
-            Statement mystmt= connection.createStatement();
+            Statement mystmt= con.createStatement();
             String sql="select *from user";
             ResultSet rs=mystmt.executeQuery(sql);
             while(rs.next()) {
                 user = new User(rs.getLong("id"), rs.getString("userName"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("password"));
                 users.add(user);
             }
-        }catch (SQLException |ClassNotFoundException ex) {
+        }catch (SQLException ex) {
             // handle any errors
             user=new User();
             user.setFirstName(ex.getMessage());
@@ -81,15 +52,10 @@ public class UserService {
     }
 
     public User findByUserName(String userName) {
-        String jdbcURL = "jdbc:mysql://localhost:3306/twitter";
-        String usernameConnection = "adrian.lupu";
-        String passwordConnection = "password";
         users.clear();
         User user;
         try{
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(jdbcURL, usernameConnection, passwordConnection);
-            Statement mystmt= connection.createStatement();
+            Statement mystmt= con.createStatement();
             String sql="select *from user WHERE userName='"+userName+"'";
             ResultSet rs=mystmt.executeQuery(sql);
             if(rs.next()){
@@ -99,7 +65,7 @@ public class UserService {
                     .findFirst()
                     .orElse(null);}
         }
-        catch (SQLException |ClassNotFoundException ex) {
+        catch (SQLException ex) {
             // handle any errors
             user=new User();
             user.setFirstName(ex.getMessage());
@@ -109,10 +75,27 @@ public class UserService {
         return user;
     }
     public User findById(Long id) {
-        return users.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new NotFoundException("user", id));
+        users.clear();
+        User user;
+        try{
+            Statement mystmt= con.createStatement();
+            String sql="select *from user WHERE id='"+id+"'";
+            ResultSet rs=mystmt.executeQuery(sql);
+            if(rs.next()){
+                user = new User(rs.getLong("id"), rs.getString("userName"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("email"), rs.getString("password"));
+                users.add(user);
+                return users.stream()
+                        .findFirst()
+                        .orElseThrow(() -> new NotFoundException("user", id));}
+        }
+        catch (SQLException ex) {
+            // handle any errors
+            user=new User();
+            user.setFirstName(ex.getMessage());
+            System.out.println("SQLException: {}" + ex.getMessage());
+        }
+        user=new User();
+        return user;
     }
 
 }
